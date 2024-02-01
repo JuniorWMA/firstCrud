3bafc9f0f1b366354801e197b8789aa14d1a9442
Wellisson Junior <junior.wmoreira@gmail.com>
2024-02-01 13:40:07 -0300

    Get all courses in controllers

--- pkg/controllers/courseGetAllController.go
+++ pkg/controllers/courseGetAllController.go
@@ -1,6 +1,24 @@
 package controllers
 
-import "github.com/juniorWMA/firstCrud/pkg/models"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/juniorWMA/firstCrud/pkg/models"
+)
 
 var NewCourse models.Course
 
+func GetAllCourses(w http.ResponseWriter, r *http.Request) {
+	
+	courses := models.GetAllCourses()
+	res, err := json.Marshal(courses)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
