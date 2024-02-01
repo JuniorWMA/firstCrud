40c7cec3272ec98075133dc4e4008488ff82e7b6
Wellisson Junior <junior.wmoreira@gmail.com>
2024-02-01 14:07:39 -0300

    Create file routes and add the routes post and get

--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.1
 	github.com/jinzhu/inflection v1.0.0 // indirect
 )
---
+++ pkg/routes/courseRoutes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/juniorWMA/firstCrud/pkg/controllers"
+)
+
+var GetRoutesCourse = func(r mux.Router) {
+	r.HandleFunc("/courses", controllers.GetAllCourses).Methods("GET")
+	r.HandleFunc("/course", controllers.CreateCourse).Methods("POST")
+}
