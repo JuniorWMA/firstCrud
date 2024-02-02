ae16f37c3075a7287b30fc0526517950ce6b8c03
Wellisson Junior <junior.wmoreira@gmail.com>
2024-02-01 22:06:22 -0300

    Refactoring db

--- pkg/configs/db-config.go
+++ pkg/configs/db-config.go
@@ -1,32 +1,34 @@
 package configs
 
 import (
-	"fmt"
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/joho/godotenv"
+)
+var (
+	db *gorm.DB
 )
 
-var DB *gorm.DB
 
-func ConnectDB() {
-
-	err := godotenv.Load()
+func Connect()  {
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
+	
 	d, err := gorm.Open("mysql", os.Getenv("MYSQL_URL"))
 	if err != nil {
-		fmt.Println("Error opening DB ")
-		log.Fatal(err)
+		panic(err)
 	}
-	DB = d
+	db = d
+
 }
 
-func GetConnectDB() *gorm.DB {
-	return DB
+func GetDB() *gorm.DB {
+	return db
 }
+
